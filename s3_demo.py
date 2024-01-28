967cdc598ba9a7a57592c2e765130136ba35d158
Ryan Collingwood <ryancollingwood@gmail.com>
2024-01-28 16:50:32 +1100

    Update s3_demo.py
    - implement a naive list_files_with_prefix method. use with caution in the case of paths that have a lot of children
    - download_file now accepts a target_path rather than a hardcoded value
    - download_file will attempt to create the folders and subfolders for storing files

--- s3_demo.py
+++ s3_demo.py
@@ -1,5 +1,5 @@
 import boto3
-
+from pathlib import Path
 
 def upload_file(file_name, bucket):
     """
@@ -12,12 +12,13 @@ def upload_file(file_name, bucket):
     return response
 
 
-def download_file(file_name, bucket):
+def download_file(file_name, bucket, target_path):
     """
     Function to download a given file from an S3 bucket
     """
     s3 = boto3.resource('s3')
-    output = f"downloads/{file_name}"
+    output = f"{target_path}/{file_name}"
+    Path(output).parent.mkdir(parents=True, exist_ok=True)
     s3.Bucket(bucket).download_file(file_name, output)
 
     return output
@@ -36,4 +37,23 @@ def list_files(bucket):
     except Exception as e:
         pass
 
+    return contents
+
+def list_files_with_prefix(prefix, bucket):
+    """
+    This is a potentially dangerous and/or flaky way to do this
+    Given there could any number of items on a path, this method may:
+        - Return a LOT of results
+        - Crap itself due to the number of results
+        - Crap itself because it looses it's place
+    """
+    s3 = boto3.client('s3')
+    contents = []
+
+    s3 = boto3.resource('s3')
+    bucket_obj = s3.Bucket(name=bucket)
+    for objects in bucket_obj.objects.filter(Prefix=prefix):
+        print(objects.key)
+        contents.append(objects.key)
+    
     return contents
